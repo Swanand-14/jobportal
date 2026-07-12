12a7ad1616aec39eccb154f4eeed8d05fa9afa8d
Swanand Takalkar <takalkarswanand@gmail.com>
2026-07-12 14:55:25 +0530

    handling logging

--- src/main/java/com/example/jobportal/company/controller/CompanyController.java
+++ src/main/java/com/example/jobportal/company/controller/CompanyController.java
@@ -3,6 +3,7 @@ package com.example.jobportal.company.controller;
 import com.example.jobportal.dto.CompanyDto;
 import com.example.jobportal.company.service.ICompanyService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/companies")
 @RequiredArgsConstructor
@@ -21,6 +23,7 @@ public class CompanyController {
 
     @GetMapping(path = "/public" , version = "1.0")
     public ResponseEntity<List<CompanyDto>> getAllCompanies(){
+        log.info("getting the company list");
         List<CompanyDto> companyList = companyService.getAllCompanies();
         return ResponseEntity.ok().body(companyList);
     }
